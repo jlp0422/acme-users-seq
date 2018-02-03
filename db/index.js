448dfb25c4e54a41ac940752682c5f0c4478a8a2
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-02-03 15:59:19 -0500

    minor update to residence

--- db/index.js
+++ db/index.js
@@ -16,8 +16,8 @@ const sync = () => {
 
 const seed = () => {
   return Promise.all([
-    User.create({ name: 'Spongebob', residence: 'pineapple', funFact: `Has won 374 consecutive 'Employee of the Month' awards`, image: '../vendor/spongebob.png' }),
-    User.create({ name: 'Patrick', residence: 'rock', funFact: 'Thinks mayonnaise is an instrument', image: '../vendor/patrick.png'}),
+    User.create({ name: 'Spongebob', residence: 'Pineapple', funFact: `Has won 374 consecutive 'Employee of the Month' awards`, image: '../vendor/spongebob.png' }),
+    User.create({ name: 'Patrick', residence: 'Rock', funFact: 'Thinks mayonnaise is an instrument', image: '../vendor/patrick.png'}),
     User.create({ name: 'Squidward', residence: 'Easter Island head', funFact: 'Is actually an octopus', image: '../vendor/squidward.png' })
   ]);
 };
